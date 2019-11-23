b01d202a5ecbe7e56ac99970cee53d661f87df61
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-11-22 20:05:55 -0500

    initial commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,36 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: []
+        value: this.props.value
     };
     
-    renderTags(){
-        if(this.state.tags.length === 0) return <p>There are no tags</p>;
+   
+    handleIncremt = product =>{
+        this.setState({value: this.state.value + 1})
+    };
 
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-    }
 
     render(){
-        return <div>{this.renderTags()}</div>
+
+        return (
+            <div>
+ 
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={() => this.handleIncremt(1)}  className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+        );
+    }
+
+
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += this.state.value === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const {value} = this.state;
+        return value === 0 ? "Zero" : value;
     }
 }
 
