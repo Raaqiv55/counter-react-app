626b3169a0e7a8b227b4c9684827aff7f9d1ab60
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-11-24 19:08:11 -0500

    initial commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        value: this.props.value
+        value: this.props.counter.value
     };
     
    
@@ -18,6 +18,7 @@ class Counter extends Component {
  
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={() => this.handleIncremt(1)}  className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,11 +11,15 @@ class Counters extends Component {
         ]
     };
 
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters});
+    };
+
     render(){
         return (
             <div>
-              {this.state.counters.map(counter => ( <Counter key={counter.id} value={counter.value} id={counter.id}>
-              </Counter>
+              {this.state.counters.map(counter => ( <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />
               ))}
             </div>
         );
